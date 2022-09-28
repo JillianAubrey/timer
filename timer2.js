32193605063cc0b13f8adb25f9d5056da555c81d
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 00:52:41 +0000

    Implemented ability to user to set a timer by inputting a number key

--- timer2.js
+++ timer2.js
@@ -5,11 +5,18 @@ stdin.setEncoding('utf8');
 stdin.on('data', (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
-    console.log();
     process.exit();
   }
   if (key === 'b') {
     process.stdout.write('\x07');
     console.log('BEEP');
+    return;
+  }
+  if (!isNaN(key)) {
+    console.log(`Setting timer for ${key} seconds...`)
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`${key} second timer is up!`)
+    }, key * 1000);
   }
 });
