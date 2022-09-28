15b586269c12f79c844d716509f3537645f39f75
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 00:53:45 +0000

    Completed activity with addition of goodbye message

--- timer2.js
+++ timer2.js
@@ -5,6 +5,7 @@ stdin.setEncoding('utf8');
 stdin.on('data', (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
     process.exit();
   }
   if (key === 'b') {
@@ -13,10 +14,10 @@ stdin.on('data', (key) => {
     return;
   }
   if (!isNaN(key)) {
-    console.log(`Setting timer for ${key} seconds...`)
+    console.log(`Setting timer for ${key} seconds...`);
     setTimeout(() => {
       process.stdout.write('\x07');
-      console.log(`${key} second timer is up!`)
+      console.log(`${key} second timer is up!`);
     }, key * 1000);
   }
 });
